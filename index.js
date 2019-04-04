5bf684b1a45570be131372a9503315145188f997
Ellyn Anderson <eka249@nyu.edu>
2019-04-04 01:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function logShout(string){
   console.log(string.toUpperCase())
 }
 logShout('hello')
+
+function sayHiToGrandma(string){
+  if string==string.toLowerCase()
+  var hear= "I can/'t hear you!",
+  if string ==string.toUpperCase(),
+  var hear= "YES INDEED!"
+  return var
+}
