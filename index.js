2dc44b624ddb97baa14dcc68b9d77073fff90710
Ellyn Anderson <eka249@nyu.edu>
2019-04-04 01:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ logShout('hello')
 
 function sayHiToGrandma(string){
   if (string==string.toLowerCase()) {
-    var hear= "I can/'t hear you!"}
+    var hear= "I can\'t hear you!"}
   if (string ==string.toUpperCase()){
-    var hear= "YES INDEED!"
-  }
+    var hear= "YES INDEED!" }
+    if(string =="I love you, Grandma."){
+      var hear= 'I love you too.'
+    }
   return hear
 }
 sayHiToGrandma('hello')
